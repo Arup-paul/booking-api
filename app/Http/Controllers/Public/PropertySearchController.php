5d96f0716286aa328971ee65a7b677c6f31fce05
arup-paul <aruppaulctg@gmail.com>
2023-04-07 23:50:56 +0600

    search

--- app/Http/Controllers/Public/PropertySearchController.php
+++ app/Http/Controllers/Public/PropertySearchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Public;
 
 use App\Http\Controllers\Controller;
+use App\Models\Geoobject;
 use App\Models\Property;
 use Illuminate\Http\Request;
 
@@ -13,6 +14,23 @@ class PropertySearchController extends Controller
         return Property::with('city')
             ->when($request->city, function($query) use ($request) {
                 $query->where('city_id', $request->city);
+            })
+            ->when($request->country, function($query) use ($request) {
+                $query->whereHas('city', fn($q) => $q->where('country_id', $request->country));
+            })
+            ->when($request->geoobject, function($query) use ($request) {
+                $geoobject = Geoobject::find($request->geoobject);
+                if ($geoobject) {
+                    $condition = "(
+                        6371 * acos(
+                            cos(radians(" . $geoobject->lat . "))
+                            * cos(radians(`lat`))
+                            * cos(radians(`long`) - radians(" . $geoobject->long . "))
+                            + sin(radians(" . $geoobject->lat . ")) * sin(radians(`lat`))
+                        ) < 10
+                    )";
+                    $query->whereRaw($condition);
+                }
             })
             ->get();
     }
--- tests/Feature/PropertiesTest.php
+++ tests/Feature/PropertiesTest.php
@@ -3,6 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\City;
+use App\Models\Country;
+use App\Models\Geoobject;
+use App\Models\Property;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -41,5 +44,50 @@ class PropertiesTest extends TestCase
 
         $response->assertSuccessful();
         $response->assertJsonFragment(['name' => 'My property']);
+    }
+
+    public function test_property_search_by_country_returns_correct_results(): void
+    {
+        $owner = User::factory()->create(['role_id' => Role::ROLE_OWNER]);
+        $countries = Country::with('cities')->take(2)->get();
+        $propertyInCountry = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $countries[0]->cities()->value('id')
+        ]);
+        $propertyInAnotherCountry = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $countries[1]->cities()->value('id')
+        ]);
+
+        $response = $this->getJson('/api/search?country=' . $countries[0]->id);
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(1);
+        $response->assertJsonFragment(['id' => $propertyInCountry->id]);
+    }
+
+    public function test_property_search_by_geoobject_returns_correct_results(): void
+    {
+        $owner = User::factory()->create(['role_id' => Role::ROLE_OWNER]);
+        $cityId = City::value('id');
+        $geoobject = Geoobject::first();
+        $propertyNear = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $cityId,
+            'lat' => $geoobject->lat,
+            'long' => $geoobject->long,
+        ]);
+        $propertyFar = Property::factory()->create([
+            'owner_id' => $owner->id,
+            'city_id' => $cityId,
+            'lat' => $geoobject->lat + 10,
+            'long' => $geoobject->long - 10,
+        ]);
+
+        $response = $this->getJson('/api/search?geoobject=' . $geoobject->id);
+
+        $response->assertStatus(200);
+        $response->assertJsonCount(1);
+        $response->assertJsonFragment(['id' => $propertyNear->id]);
     }
 }
