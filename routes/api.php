69024eaf8da635e32b149e7c730c268d60707037
arup-paul <aruppaulctg@gmail.com>
2023-04-09 23:43:37 +0600

    aprtment

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Owner\PropertyController;
+use App\Http\Controllers\Public\PropertyController as PublicPropertyController;
+use App\Http\Controllers\Public\ApartmentController;
+use App\Http\Controllers\Public\PropertySearchController;
 use App\Http\Controllers\User\BookingController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 
 
 Route::middleware('auth:sanctum')->group(function() {
-    // No owner/user grouping, for now, will do it later with more routes
     Route::get('owner/properties',
         [PropertyController::class, 'index']);
     Route::post('owner/properties',
@@ -27,9 +19,8 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::get('user/bookings',
         [BookingController::class, 'index']);
 });
-Route::get('search',
-    \App\Http\Controllers\Public\PropertySearchController::class);
-Route::get('properties/{property}',
-    \App\Http\Controllers\Public\PropertyController::class);
+Route::get('search',PropertySearchController::class);
+Route::get('properties/{property}',PublicPropertyController::class);
+Route::get('apartments/{apartment}',ApartmentController::class);
 
-Route::post('auth/register', App\Http\Controllers\Auth\RegisterController::class);
+Route::post('auth/register', RegisterController::class);
